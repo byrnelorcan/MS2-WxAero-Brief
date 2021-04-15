af7fb4f4037bef3e268daeb280f10970c4524648
byrnelorcan <lorcan.byrne@icloud.com>
2021-04-15 18:46:29 +0000

    add taf data and structure for calls on timestamps

--- assets/js/checkwx.js
+++ assets/js/checkwx.js
@@ -76,4 +76,47 @@ $(function () {
             $('results_block').removeClass('d-none');
         }
     });
+    /*----------------------------- TAF DATA -----------------------------------*/
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.checkwx.com/taf/' + icao + '/decoded',
+        headers: {
+            'X-API-Key': '4a6fff29393d4f7080abbdfae4'
+        },
+        dataType: 'json',
+
+        error: function (response) {
+            var e = response.responseJSON;
+            $('#icao').text(e.StatusCode);
+            $('#name').text(e.error);
+            $('#raw').text(e.message);
+        },
+
+        success: function (response) {
+            if (response.results > 0) {
+                var taf = response.data[0];
+
+                if (taf.forecast) {
+                    var forecast1 = taf.forecast[0];
+                    $('#timestamp_1').html('Forecast from ' + forecast1.timestamp.from + ' to ' + forecast1.timestamp.to);
+                    var forecast2 = taf.forecast[1];
+                    $('#timestamp_2').html(forecast2.change.indicator.text + ' from ' + forecast2.timestamp.from + ' to ' + forecast2.timestamp.to);
+                    var forecast3 = taf.forecast[2];
+                    $('#timestamp_3').html(forecast3.change.indicator.text + ' from ' + forecast3.timestamp.from + ' to ' + forecast3.timestamp.to);
+                    var forecast4 = taf.forecast[3];
+                    $('#timestamp_4').html(forecast4.change.indicator.text + ' from ' + forecast4.timestamp.from + ' to ' + forecast4.timestamp.to);
+                    var forecast5 = taf.forecast[4];
+                    $('#timestamp_5').html(forecast5.change.indicator.text + ' from ' + forecast5.timestamp.from + ' to ' + forecast5.timestamp.to);
+                }
+                //taf.forecast.forEach(forecast => {
+                //    $('#timestamp_1').append('Forecast from' + forecast.timestamp.from + 'to' + forecast.timestamp.to);
+                //})
+                //}
+                //if (taf.forecast[0]) {
+                //    $('#timestamp_1').text(forecast.timestamp.from);
+                //}
+
+            }
+        }
+    });
 });
